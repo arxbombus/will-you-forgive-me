2cb8f56624d54ba1b91632c67b76df9d182bbe94
arxbombus <arxbombus@gmail.com>
2026-02-06 11:06:54 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { myTestFunc } from "~/test";
+import viteLogo from "/vite.svg";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [countMod, setCountMod] = useState(0);
+
+  const handleCountIncrement = () => {
+    const newCount = myTestFunc(count, 10);
+    setCount(newCount);
+    setCountMod(newCount % 2);
+  };
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" rel="noopener" target="_blank">
+          {/** biome-ignore lint/correctness/useImageSize: 1 */}
+          <img alt="Vite logo" className="logo" src={viteLogo} />
+        </a>
+        <a href="https://react.dev" rel="noopener" target="_blank">
+          <img alt="React logo" className="logo react" src={reactLogo} />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={handleCountIncrement} type="button">
+          count is {count}
+        </button>
+        <p>{countMod}</p>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
